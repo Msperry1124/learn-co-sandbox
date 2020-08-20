54516957f5b5e30b91e31a8f234e6b0723bf62a0
Melissa Sperry <sperry.mel@gmail.com>
2020-08-20 12:48:18 +0000

    Automatically backed up by Learn

---
+++ New_Release_Books/lib/New_Release_Books.rb
@@ -0,0 +1,6 @@
+require "New_Release_Books/version"
+
+module NewReleaseBooks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ New_Release_Books/lib/New_Release_Books/version.rb
@@ -0,0 +1,3 @@
+module NewReleaseBooks
+  VERSION = "0.1.0"
+end
